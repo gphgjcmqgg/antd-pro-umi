036ab7cecf26e3ad04c57752cb0dc2c04d196cda
gphgjcmqgg <gphgjcmqgg@sina.com>
2020-02-22 18:20:33 +0800

    welconme 精简

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,17 +1,6 @@
 import React from 'react';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
-import { FormattedMessage } from 'umi-plugin-react/locale';
-import { Card, Typography, Alert } from 'antd';
-
-import styles from './Welcome.less';
-
-const CodePreview: React.FC<{}> = ({ children }) => (
-  <pre className={styles.pre}>
-    <code>
-      <Typography.Text copyable>{children}</Typography.Text>
-    </code>
-  </pre>
-);
+// import styles from './Welcome.less';
 
 export default (): React.ReactNode => (
   <PageHeaderWrapper>
